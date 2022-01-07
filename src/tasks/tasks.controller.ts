e16abb357a32f00bded3a426ea88a5dafcf19efa
Ellery Vernon <tmpks3@gmail.com>
2022-01-07 09:26:37 +0900

    Add: getAllTasks function

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,12 @@
 import { Controller } from '@nestjs/common';
+import { Task } from './tasks.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+}
